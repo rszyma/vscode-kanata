26dad582fa9cb2002470ad84fd2296fbcb61714c
rszyma <rszyma.dev@gmail.com>
2024-02-05 15:51:25 +0100

    yarn run fmtwrite

--- test/src/runTest.ts
+++ test/src/runTest.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
+import { resolve } from "path";
 
-import { runTests } from '@vscode/test-electron'; // eslint-disable-line node/no-unpublished-import
-import { minVersion } from 'semver'; // eslint-disable-line node/no-unpublished-import
+import { runTests } from "@vscode/test-electron";
+import { minVersion } from "semver";
 
-import { engines } from '../../package.json';
+import { engines } from "../../package.json";
 
 void (async function () {
   try {
@@ -12,20 +12,21 @@ void (async function () {
     const minSupportedVSCodeVersion =
       minVersion(engines.vscode)?.toString() || engines.vscode;
 
-    const extensionDevelopmentPath = resolve(__dirname, '../../..');
-    const extensionTestsPath = resolve(__dirname, './suite');
+    const extensionDevelopmentPath = resolve(__dirname, "../../..");
+    const extensionTestsPath = resolve(__dirname, "./suite");
     const workspace = resolve(
       __dirname,
-      '../../../test-fixtures/workspace/test.code-workspace',
+      "../../../test-fixtures/workspace/test.code-workspace",
     );
 
     await runTests({
       version: minSupportedVSCodeVersion,
       extensionDevelopmentPath,
       extensionTestsPath,
-      launchArgs: [workspace, '--disable-extensions', '--disable-telemetry'],
+      launchArgs: [workspace, "--disable-extensions", "--disable-telemetry"],
     });
-  } catch (e) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
     console.error(`====== ERROR ======\n${e}\n==================`);
     process.exitCode = 1;
   }
